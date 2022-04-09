5d5e2d1a04acc323eb98d2d29b8e36495bf19db2
Thomas Sommer <38225915+DerbTriumph@users.noreply.github.com>
2022-04-09 12:10:41 +0200

    Spielfeld (#5)
    Ales Top

--- M426Game/Gamefield.java
+++ M426Game/Gamefield.java
@@ -1,13 +1,17 @@
+import java.util.*;
 class Gamefield extends MyPApplet {
   
   final Player player = new Player(30);
   Score score = new Score();
   ColisionDetection colision = new ColisionDetection();
   String state = "start";
+  int[][] Gamefield = new int[16][9];
+  int[] ReiheY = new int[9];
   
   void drawfield(){
       p.fill(10,255,32);
       player.show();
+      createInitalField(ReiheY, Gamefield);
       String px = String.valueOf(player.xPos);
       p.println(px);
       state = colision.playerOutOfBounds(player.xPos);
@@ -37,7 +41,42 @@ class Gamefield extends MyPApplet {
         player.left();
       break;
      
+      case 'p':
+      case 'P':
+        state = "pause";
+      break;
+     
       default: break;
+    }
+  }
+  
+  void createInitalField(int[] reiheY, int[][] gamefield){
+    for(int u=0; u<3; u+=2){
+        Random rand = new Random();
+        int upperbound = 2; // befüllt(1) oder nicht befüllt(0)
+        int upperboundPosFree = 10; // Plätze in einer Reihe Y
+        int posFree1 = rand.nextInt(upperboundPosFree);
+        int posFree2 = rand.nextInt(upperboundPosFree);
+        
+        for(int i = 0; i < 7; i++){
+          if (i != posFree1 || i != posFree2){
+            int wert = rand.nextInt(upperbound); 
+              reiheY[i] = wert;
+          }else{
+              reiheY[i] = 0;
+          }
+        }
+        
+        // ReiheY in das Spielfeld übertragen
+        for(int i = 0; i<9; i++){
+           gamefield[13+u][i] = reiheY[i];
+        }
+     }
+    
+    for(int e = 0; e < 16; e++) {
+         for(int r = 0; r < 9; r++) {
+           System.out.println(gamefield[e][r]);
+         }
     }
   }
 }
